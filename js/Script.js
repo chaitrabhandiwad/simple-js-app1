53304f9ebef265eba7bbd0a46cb6e743da9ebb2d
chaitrabhandiwad <chaitrabhandiwad@users.noreply.github.com>
2019-11-14 17:49:46 +0100

    Adding objects to array in Javascript.

--- js/Script.js
+++ js/Script.js
@@ -1,3 +1,11 @@
-alert('Hello World');
-var favouriteFood ='favouriteFood = Burger';
-document.write(favouriteFood);
+var pokemonrepository = [
+  { name: "Bulbasor",moves:['poisonpowder','leechseed'], height:1},
+  { name: "Jigglypuff",moves: ['sing','quiverattack'], height:1},
+  { name: "Pichachu", moves: ['electricshock','thundervolt'], height:1},
+  { name:"Baylee", moves:['rasorleaf','quiverattack'], height:1},
+  { name:"Butterfree", moves:['confusion','whirlwind'], height:1},
+  { name:"Charmander", moves:['fireball','inferno'], height:4},
+  { name:"Squirrtel", moves:['waterspray','watercyclone'], height:1}
+];
+
+console.log(pokemonrepository[]);
