2654a89cde7c8d31cfb6254303218d2abd4d8a1b
chaitrabhandiwad <chaitrabhandiwad@users.noreply.github.com>
2019-11-17 21:26:04 +0100

    Excercise 1.3 Javascript Loops and Conditions

--- js/Script.js
+++ js/Script.js
@@ -1,11 +1,28 @@
-var pokemonrepository = [
-  { name: "Bulbasor",moves:['poisonpowder','leechseed'], height:1},
-  { name: "Jigglypuff",moves: ['sing','quiverattack'], height:1},
-  { name: "Pichachu", moves: ['electricshock','thundervolt'], height:1},
-  { name:"Baylee", moves:['rasorleaf','quiverattack'], height:1},
-  { name:"Butterfree", moves:['confusion','whirlwind'], height:1},
-  { name:"Charmander", moves:['fireball','inferno'], height:4},
-  { name:"Squirrtel", moves:['waterspray','watercyclone'], height:1}
-];
 
-console.log(pokemonrepository[]);
+var pokemonrepository = [
+  { name: "Bulbasaur",moves:['poisonpowder','leechseed'], height:1,rank:1},
+  { name: "Jigglypuff",moves: ['sing','quiverattack'], height:1, rank:39},
+  { name: "Pikachu", moves: ['electricshock','thundervolt'], height:1, rank:25},
+  { name:"Bayleef", moves:['rasorleaf','quiverattack'], height:1, rank:153},
+  { name:"Butterfree", moves:['confusion','whirlwind'], height:1, rank:12},
+  { name:"Charmander", moves:['fireball','inferno'], height:4, rank:4},
+  { name:"Squirtel", moves:['waterspray','watercyclone'], height:1, rank:6}
+];
+/*
+var myImage = document.getElementByid('mainImage');
+var imageArray =["Bulbasaur.jpg","Jigglypuff.jpg","Pikachu.png","Bayleef.jpg","Butterfree.jpg","Charmander.jpg","Squirtel.jpg"];*/
+
+//myImage.setAttribute("src", imageArray[1]);
+
+for(i=0;i<=pokemonrepository.length;i++)
+{
+
+  document.write('<p>' + pokemonrepository[i].name + '(height:'  + pokemonrepository[i].height +  ')</p>')
+  if(pokemonrepository[i].rank === 153)
+  {
+    document.write( "Wow thats big!!!!!")
+    
+    //document.write('<p>' + pokemonrepository[i].name '</p>')
+  //  document.write('<p>' + imageArray[i].src +  '</p>')
+     }
+}
