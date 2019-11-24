99496f4b84e8d16b0d17dddc5e3db0fe9d4d7b4f
chaitrabhandiwad <chaitrabhandiwad@users.noreply.github.com>
2019-11-24 15:15:40 +0100

    Excercise 1.4 Javascript

--- js/Script.js
+++ js/Script.js
@@ -13,7 +13,7 @@ var myImage = document.getElementByid('mainImage');
 var imageArray =["Bulbasaur.jpg","Jigglypuff.jpg","Pikachu.png","Bayleef.jpg","Butterfree.jpg","Charmander.jpg","Squirtel.jpg"];*/
 
 //myImage.setAttribute("src", imageArray[1]);
-
+/*
 for(i=0;i<=pokemonrepository.length;i++)
 {
 
@@ -25,4 +25,25 @@ for(i=0;i<=pokemonrepository.length;i++)
     //document.write('<p>' + pokemonrepository[i].name '</p>')
   //  document.write('<p>' + imageArray[i].src +  '</p>')
      }
-}
+}*/
+pokemonrepository.forEach(function(currentPokemon) {
+  var pokemon = document.createElement("div");
+
+  var pokemonTitle = document.createElement("h1");
+  if (currentPokemon.height > 3) {
+    pokemonTitle.textContent =
+      currentPokemon.name +
+      " (height: " +
+      currentPokemon.height +
+      ") " +
+      "- Wow, that's big! ')";
+    pokemon.setAttribute("class", "grid_item_darken");
+  } else {
+    pokemonTitle.textContent =
+      currentPokemon.name + " (height: " + currentPokemon.height + ")";
+    pokemon.setAttribute("class", "grid__item");
+  }
+
+  document.getElementById("pokemon-container").appendChild(pokemon);
+  pokemon.appendChild(pokemonTitle);
+});
